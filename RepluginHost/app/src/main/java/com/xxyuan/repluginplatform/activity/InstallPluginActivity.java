f6f14e3fc26bc78ac6d02e3ce78333ec38c4c945
xiaoyuan916 <402973804@qq.com>
2019-01-18 17:12:21 +0800

    修改安装逻辑

--- RepluginHost/app/src/main/java/com/xxyuan/repluginplatform/activity/InstallPluginActivity.java
+++ RepluginHost/app/src/main/java/com/xxyuan/repluginplatform/activity/InstallPluginActivity.java
@@ -12,6 +12,7 @@ import com.qihoo360.replugin.RePlugin;
 import com.qihoo360.replugin.model.PluginInfo;
 import com.qihoo360.replugin.utils.FileUtils;
 import com.xxyuan.repluginplatform.R;
+import com.xxyuan.repluginplatform.utils.InstallPluginUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,81 +35,18 @@ public class InstallPluginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_install_plugin);
         ButterKnife.bind(this);
     }
-    @OnClick({R.id.bt_install_plugin,R.id.bt_start_plugin})
+
+    @OnClick({R.id.bt_install_plugin, R.id.bt_start_plugin})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.bt_install_plugin:
-                simulateInstallExternalPlugin("replugin.apk");
+                String apkPath = "external" + File.separator + "replugin.apk";
+                InstallPluginUtils.startRePlugin(this,"replugin",apkPath);
                 break;
             case R.id.bt_start_plugin:
                 RePlugin.startActivity(this,
                         RePlugin.createIntent("replugin", "com.xxyuan.replugin.MainActivity"));
                 break;
-        }
-    }
-
-
-
-
-    /**
-     * 模拟安装或升级（覆盖安装）外置插件
-     * 注意：为方便演示，外置插件临时放置到Host的assets/external目录下，具体说明见README</p>
-     */
-    private void simulateInstallExternalPlugin(String apkName) {
-        String demo3Apk = apkName;
-        String demo3apkPath = "external" + File.separator + demo3Apk;
-
-        // 文件是否已经存在？直接删除重来
-        String pluginFilePath = getFilesDir().getAbsolutePath() + File.separator + demo3Apk;
-        File pluginFile = new File(pluginFilePath);
-        if (pluginFile.exists()) {
-            FileUtils.deleteQuietly(pluginFile);
-        }
-
-        // 开始复制
-        copyAssetsFileToAppFiles(demo3apkPath, demo3Apk);
-        PluginInfo info = null;
-        if (pluginFile.exists()) {
-            info = RePlugin.install(pluginFilePath);
-        }
-
-        if (info != null) {
-//            RePlugin.startActivity(this, RePlugin.createIntent(info.getName(), "com.qihoo360.replugin.sample.demo3.MainActivity"));
-            Toast.makeText(this, "install external plugin success", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "install external plugin failed", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    /**
-     * 从assets目录中复制某文件内容
-     *
-     * @param assetFileName assets目录下的Apk源文件路径
-     * @param newFileName   复制到/data/data/package_name/files/目录下文件名
-     */
-    private void copyAssetsFileToAppFiles(String assetFileName, String newFileName) {
-        InputStream is = null;
-        FileOutputStream fos = null;
-        int buffsize = 1024;
-
-        try {
-            is = this.getAssets().open(assetFileName);
-            fos = this.openFileOutput(newFileName, Context.MODE_PRIVATE);
-            int byteCount = 0;
-            byte[] buffer = new byte[buffsize];
-            while ((byteCount = is.read(buffer)) != -1) {
-                fos.write(buffer, 0, byteCount);
-            }
-            fos.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-                fos.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 }
